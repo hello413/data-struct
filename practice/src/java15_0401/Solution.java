34ae1b01e3bb9f1c70d6b43b80584d2d0603bdbe
hello413 <2827717859@qq.com>
2020-04-02 11:32:43 +0800

    practical

---
+++ practice/src/java15_0401/Solution.java
@@ -0,0 +1,39 @@
+package java15_0401;
+
+import java.util.Stack;
+
+class Solution {
+    // 栈实现   8 ms	36.7 MB
+    public static int evalRPN(String[] tokens) {
+        Stack<Integer> numStack = new Stack<>();
+        Integer op1, op2;
+        for (String s : tokens) {
+            switch (s) {
+                case "+":
+                    op2 = numStack.pop();
+                    op1 = numStack.pop();
+                    numStack.push(op1 + op2);
+                    break;
+                case "-":
+                    op2 = numStack.pop();
+                    op1 = numStack.pop();
+                    numStack.push(op1 - op2);
+                    break;
+                case "*":
+                    op2 = numStack.pop();
+                    op1 = numStack.pop();
+                    numStack.push(op1 * op2);
+                    break;
+                case "/":
+                    op2 = numStack.pop();
+                    op1 = numStack.pop();
+                    numStack.push(op1 / op2);
+                    break;
+                default:
+                    numStack.push(Integer.valueOf(s));
+                    break;
+            }
+        }
+        return numStack.pop();
+    }
+}
