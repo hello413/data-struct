3c90eb445c6c00e26fc98b7a9e0a6f699e5090c7
hello413 <2827717859@qq.com>
2020-06-04 15:21:58 +0800

    practical

--- practice/src/java_0604/Main.java
+++ practice/src/java_0604/Main.java
@@ -10,7 +10,8 @@ public class Main {
         int count = 0;
         char [] arr = string.toCharArray();
         for (int i= 0 ;i<arr.length;i++){
-            if('0'<=arr[i] && '9'>= arr[i]){//当前的是数字
+            char ch = arr[i];
+            if('0'<=ch && '9'>= ch){//当前的是数字
                 count = 1;//初始化计算器
                 int index = i;//在后面的循环存储截至索引
                 for(int j=i+1;j<arr.length;j++){
