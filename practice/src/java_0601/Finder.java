3b317f19537c71b9b547538d7ec499a35477b1a6
hello413 <2827717859@qq.com>
2020-06-05 16:19:25 +0800

    practical

--- practice/src/java_0601/Finder.java
+++ practice/src/java_0601/Finder.java
@@ -8,7 +8,30 @@ public class Finder {
     }
     public static int findKth(int[] a, int n, int K) {
         // write code here
-        Arrays.sort(a);
-        return a[K-1];
+        quicksorthelp(a,0,n);
+        return a[K];
+    }
+
+    private static void quicksorthelp(int[] a, int left, int right) {
+        if (left>=right){
+            return;
+        }
+        int index = paintition(a,left,right);
+        quicksorthelp(a,left,index-1);
+        quicksorthelp(a,index+1,right);
+    }
+
+    private static int paintition(int[] a, int left, int right) {
+        int begin = left;
+        int end = right;
+        int temp =a[right];
+        while (begin<end){
+            while (begin<end&&a[begin]>=temp){
+                begin++;
+            }
+            while (begin<end&&a[begin]<=a[end]){
+                end--;
+            }
+        }
     }
 }
