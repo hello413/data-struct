2744c93de6b40ee54a54f5c06b611e2942717255
hello413 <2827717859@qq.com>
2020-07-09 17:56:02 +0800

    practical

--- practice/src/java_0709/Main.java
+++ practice/src/java_0709/Main.java
@@ -1,6 +1,8 @@
+package java_0709;
+
 import java.util.Scanner;
 
-public class Main{
+public class Main {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
