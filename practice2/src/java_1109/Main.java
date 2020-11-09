26742a02758257cd4ab8ff55f1d4f0565da4e826
hello413 <2827717859@qq.com>
2020-11-09 11:37:36 +0800

    practical

---
+++ practice2/src/java_1109/Main.java
@@ -0,0 +1,17 @@
+package java_1109;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        int m = 4,n=4;
+        int[][] arr = new int[m][n];
+        boolean[][] flag = new boolean[m][n];
+        int k = 4;
+        Random random = new Random();
+        while (k-->0){
+            int x = random.nextInt(4);
+            int y = random.nextInt(4);
+        }
+    }
+}
